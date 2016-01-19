388a6ee8405def1d23f3d7fda913ccde07fe4d41
Nick Bolten <nbolten@gmail.com>
2016-01-19 13:02:29 -0800

    Version bump prior to release, added package information to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+'''Coral: code-ify your synthetic DNA design workflow.
+
+Documentation available at http://coral.readthedocs.org.
+
+Coral is a library for encoding the process of designing synthetic DNA
+constructs. Coral mirrors the traditional design steps used in GUI-based
+sequence design (ApE, j5, Benchling, etc.) as operations on data structures,
+enables iterative design through analysis modules, and connects seamlessly to
+outside libraries. Through the use of Coral, you can translate your DNA design
+processes into concise, executable, and reusable scripts.
+
+Coral encodes synthetic DNA design rules into its core sequence data types
+(DNA, RNA, and Peptide), enabling concise, dependable methods for automated
+DNA design.
+
+Coral works with PyPy so long as a PyPy-compatible numpy is installed.
+'''
+
 # Check python versions
 import sys
 if sys.version_info.major > 2:
@@ -9,15 +27,23 @@ try:
 except ImportError:
     from distutils.core import setup, Extension
 
-import numpy
+import numpy  # noqa
+
+
+doclines = __doc__.split('\n')
 
 config = {
-    'description': 'coral',
+    'name': 'coral',
+    'version': '0.3.4',
+    'description': doclines[0],
+    'long_description': '\n'.join(doclines[2:]),
     'author': 'Nick Bolten',
-    'url': 'https://github.com/klavinslab/coral',
-    'download_url': 'https://github.com/klavinslab/coral.git',
     'author_email': 'nbolten _at_ gmail',
-    'version': '0.3.3',
+    'maintainer': 'Nick Bolten',
+    'maintainer_email': 'nbolten _at_ gmail',
+    'url': 'https://github.com/klavinslab/coral',
+    'license': 'MIT',
+    'download_url': 'https://github.com/klavinslab/coral.git',
     'install_requires': ['numpy', 'biopython'],
     'extras_require': {'plotting': ['matplotlib'],
                        'yeastdatabases': ['intermine', 'requests'],
@@ -39,11 +65,11 @@ config = {
                                'coral/sequence/d3-plasmid.js']},
     'include_package_data': True,
     'scripts': [],
-    'name': 'coral',
-    'license': 'MIT',
     'classifiers': ['Programming Language :: Python',
                     'Programming Language :: Python :: 2.7',
-                    'Programming Language :: Python :: 2 :: Only']
+                    'Programming Language :: Python :: 2 :: Only',
+                    'Topic :: Scientific/Engineering',
+                    'Topic :: Scientific/Engineering :: Bio-Informatics']
 }
 
 seq_extension = Extension('coral.analysis._sequencing.calign',
