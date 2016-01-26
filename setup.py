2a52034044a9cde6cb27c93d7ff43b9c16afbf55
Nick Bolten <nbolten@gmail.com>
2016-01-25 17:50:53 -0800

    Commit before 0.4.0 release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ doclines = __doc__.split('\n')
 
 config = {
     'name': 'coral',
-    'version': '0.3.4',
+    'version': '0.4.0',
     'description': doclines[0],
     'long_description': '\n'.join(doclines[2:]),
     'author': 'Nick Bolten',
