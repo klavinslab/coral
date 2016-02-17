5891f773dfc7ca7aedddb374c6b10d1fbf920046
Nick Bolten <nbolten@gmail.com>
2016-02-17 01:12:13 -0800

    Removed redundant constants submodules

--- coral/constants/__init__.py
+++ coral/constants/__init__.py
@@ -1,5 +1,3 @@
 '''Convenient data files for use in other modules.'''
-from . import alphabets
-from . import complements
 from . import genbank
 from .restriction_sites import fallback_enzymes
--- coral/constants/complements.py
+++
@@ -1,6 +0,0 @@
-'''Defines alphabest available for sequences.'''
-
-
-COMPLEMENTS = {'dna': {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', 'N': 'N',
-                       '-': '-'},
-               'rna': {'A': 'U', 'U': 'A', 'G': 'C', 'C': 'G', 'N': 'N'}}
